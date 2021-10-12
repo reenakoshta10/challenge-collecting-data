26e376d4ce26d561d0d8f69e421c025dc979b945
Yusuf Akcakaya <yusufakcakaya14@gmail.com>
2021-10-12 14:37:13 +0200

    wtited prices to dict.

--- immo_scraping/web_scraping.py
+++ immo_scraping/web_scraping.py
@@ -4,13 +4,11 @@ from selenium import webdriver
 import json
 import pandas as pd
 
-all_urls = []
-
-driver = webdriver.Chrome(executable_path="/Users/yusufakcakaya/Desktop/chromedriver")
-for i in range(1, 334):
+list_of_properties = []
+for i in range(1, 2):
     page_num = str(i) + "&orderBy=relevance"
-    url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page="
-
+    url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=" + page_num
+    driver = webdriver.Chrome(executable_path="/Users/yusufakcakaya/Desktop/chromedriver")
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -18,8 +16,19 @@ for i in range(1, 334):
     listings = soup.find_all("a", class_="card__title-link")
 
     for pages in listings:
-        all_urls.append(pages["href"])
 
+        property_details = {}
+        driver.get(pages["href"])
+        property_details_page = BeautifulSoup(driver.page_source, "html.parser")
 
-print((len(all_urls)))
+        try:
+            element = property_details_page.find("p", class_="classified__price")
+            price = element.find_all("span")[1].text
+            property_details['price'] = price
+        except ConnectionError:
+            raise RuntimeError('Failed to open website')
 
+        list_of_properties.append(property_details)
+    print(list_of_properties)
+
+    driver.quit()
