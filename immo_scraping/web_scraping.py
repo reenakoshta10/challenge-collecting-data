87ba91b6790d57c3e34051f7e21f6077de2c4c24
Reena Koshta <reenakoshta@yahoo.com>
2021-10-12 12:03:06 +0200

    added code to get price from property details page

--- immo_scraping/web_scraping.py
+++ immo_scraping/web_scraping.py
@@ -4,13 +4,12 @@ from selenium import webdriver
 import json
 import pandas as pd
 
-all_urls = []
 
-driver = webdriver.Chrome(executable_path="/Users/yusufakcakaya/Desktop/chromedriver")
-for i in range(1, 334):
+list_of_properties = []
+for i in range(1, 3):
     page_num = str(i) + "&orderBy=relevance"
     url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&page="
-
+    driver = webdriver.Chrome(executable_path="driver/chromedriver")
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -18,8 +17,14 @@ for i in range(1, 334):
     listings = soup.find_all("a", class_="card__title-link")
 
     for pages in listings:
-        all_urls.append(pages["href"])
+        
+        property_details ={}
+        driver.get(pages["href"])
+        property_details_page= BeautifulSoup(driver.page_source, "html.parser")
+        
+        price = property_details_page.find("p", class_="classified__price")
+        property_details['price']= price
 
+        list_of_properties.append(property_details)
 
-print((len(all_urls)))
-
+    driver.quit()
